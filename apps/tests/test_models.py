0f34ffb5985cba69d5ec8e83978733c5af8fdc52
Amirhossein Dezhabdolahi <11095582-Amirhosseindzh@users.noreply.gitlab.com>
2024-05-21 19:13:47 +0330

    documents are completed.

--- apps/tests/test_models.py
+++ apps/tests/test_models.py
@@ -5,12 +5,14 @@ from models.db_handler import DatabaseConnector, Table, Email, User
 
 
 class DatabaseConnectTestCase(unittest.TestCase):
+    # Tests connection to database
     @patch("mysql.connector.connect")
     def test_connect_success(self, mock_connect):
         # Setup the mock to simulate successful connection
         mock_connection = MagicMock()
         mock_connect.return_value = mock_connection
 
+        # Create a sample connection instance and connect to it
         db = DatabaseConnector(
             host="localhost", user="root", password="password", database="test_db"
         )
@@ -31,10 +33,10 @@ class DatabaseConnectTestCase(unittest.TestCase):
         # Setup the mock to simulate a connection failure
         mock_connect.side_effect = mysql.connector.Error("Connection error")
 
+        # Create a Sample Connection instance and connect to it
         db = DatabaseConnector(
             host="localhost", user="root", password="password", database="test_db"
         )
-
         db.connect()
 
         # Check if the print function was called with the expected error message
@@ -42,7 +44,9 @@ class DatabaseConnectTestCase(unittest.TestCase):
 
 
 class DatabaseDisconnectTestCase(unittest.TestCase):
+    # This test disconnect from the database
     def test_disconnect(self):
+        # Create a sample database connection instance
         db = DatabaseConnector(
             host="localhost", user="root", password="password", database="test_db"
         )
@@ -55,6 +59,7 @@ class DatabaseDisconnectTestCase(unittest.TestCase):
         db.cursor = mock_cursor
         db.connection = mock_connection
 
+        # disconnect from the database
         db.disconnect()
         # Assert cursor.close() and connection.close() is called
         mock_cursor.close.assert_called_once()
@@ -62,9 +67,12 @@ class DatabaseDisconnectTestCase(unittest.TestCase):
 
 
 class ExecuteQueryTestCase(unittest.TestCase):
+    # Tests execute query
+    # first create a mock of connect and disconnect with decorator
     @patch.object(DatabaseConnector, "connect")
     @patch.object(DatabaseConnector, "disconnect")
     def test_execute_query_success(self, mock_disconnect, mock_connect):
+        # Create a sample database connection instance
         db = DatabaseConnector(
             host="localhost", user="root", password="password", database="test_db"
         )
@@ -77,9 +85,10 @@ class ExecuteQueryTestCase(unittest.TestCase):
         db.cursor = mock_cursor
         db.connection = mock_connection
 
+        # create a sample query and parameters
         query = "SELECT * FROM test WHERE id= %s"
         params = (1,)
-
+        # run execute_query method from the database instance
         db.execute_query(query, params)
 
         # Assert connect method is called
@@ -92,10 +101,12 @@ class ExecuteQueryTestCase(unittest.TestCase):
         # Assert the Disconnect method is called
         mock_disconnect.assert_called_once()
 
+    # first create a decorator of mock of connect and disconnect
     @patch.object(DatabaseConnector, "connect")
     @patch.object(DatabaseConnector, "disconnect")
     @patch("builtins.print")  # Mock the print function
     def test_execute_query_failure(self, mock_print, mock_disconnect, mock_connect):
+        # Create a sample database connection instance
         db = DatabaseConnector(
             host="localhost", user="root", password="password", database="test_db"
         )
@@ -110,7 +121,7 @@ class ExecuteQueryTestCase(unittest.TestCase):
         # Assign mock objects to the instance
         db.cursor = mock_cursor
         db.connection = mock_connection
-
+        # Create a sample query and parameters
         query = "SELECT * FROM users WHERE id = %s"
         params = (1,)
 
@@ -130,9 +141,12 @@ class ExecuteQueryTestCase(unittest.TestCase):
 
 
 class FetchDataTestCase(unittest.TestCase):
+    # create a fetch_data test class
+    # Create a mock object of connect and disconnect
     @patch.object(DatabaseConnector, "connect")
     @patch.object(DatabaseConnector, "disconnect")
     def test_fetch_data_success(self, mock_disconnect, mock_connect):
+        # Create a sample database connection instance
         db = DatabaseConnector(
             host="localhost", user="root", password="password", database="test_db"
         )
@@ -144,10 +158,10 @@ class FetchDataTestCase(unittest.TestCase):
         # Assign mock objects to the instance
         db.cursor = mock_cursor
         db.connection = mock_connection
-
+        # Create a sample of query and parameter
         query = "SELECT * FROM test WHERE id= %s"
         params = (1,)
-
+        # Run fetch_data method of database instance
         db.fetch_data(query, params)
 
         # Assert connect method is called
@@ -160,10 +174,13 @@ class FetchDataTestCase(unittest.TestCase):
         # Assert the Disconnect method is called
         mock_disconnect.assert_called_once()
 
+    # Create a fetch_data test class
+    # Create a mock object of connect and disconnect
     @patch.object(DatabaseConnector, "connect")
     @patch.object(DatabaseConnector, "disconnect")
     @patch("builtins.print")  # Mock the print function
     def test_fetch_data_failure(self, mock_print, mock_disconnect, mock_connect):
+        # Create a sample database connection instance
         db = DatabaseConnector(
             host="localhost", user="root", password="password", database="test_db"
         )
@@ -199,6 +216,7 @@ class FetchDataTestCase(unittest.TestCase):
 
 class TableTestCase(unittest.TestCase):
     def setUp(self):
+        # Create a sample database connection of table instance
         self.table = Table(
             host="localhost", user="root", password="password", database="test_db"
         )
@@ -208,13 +226,14 @@ class TableTestCase(unittest.TestCase):
     def test_table_exists_true(self, mock_fetch_data):
         # mock the fetch_data method to return empty results
         mock_fetch_data.return_value = [("example_table")]
-
+        # Assert if true table exists
         self.assertTrue(self.table.table_exists("non_existant_table"))
 
     @patch.object(Table, "fetch_data")
     def test_table_exists_false(self, mock_fetch_data):
+        # Create a mock fetch_data instance
         mock_fetch_data.return_value = []
-
+        # Assert flase if table exists 
         self.assertFalse(self.table.table_exists("example_table"))
 
     @patch.object(Table, "fetch_data")
@@ -228,8 +247,8 @@ class TableTestCase(unittest.TestCase):
     @patch.object(Table, "execute_query")
     def test_create_customer_table_success(self, mock_execute_query):
         # Mock execute_query to simulate a successful table creation
-        # Normally no return value for a successful CREATE TABLE 
-        mock_execute_query.return_value = (None)
+        # Normally no return value for a successful CREATE TABLE
+        mock_execute_query.return_value = None
 
         with self.assertLogs(level="INFO") as log:
             # Call the create_customers_table method
@@ -260,21 +279,25 @@ class TableTestCase(unittest.TestCase):
 
 
 class EmailTestCase(unittest.TestCase):
+    # Test email addresses is exist or not 
     def setUp(self):
+        # Create a sample database connection of email instance
         self.email = Email(
             host="localhost", user="root", password="password", database="test_db"
         )
 
     @patch.object(Email, "fetch_data")
     def test_email_exists_true(self, mock_fetch_data):
+        # Create mock instance with fake email address 
         mock_fetch_data.return_value = [("example@example.com")]
-
+        # Assert true if email address exists
         self.assertTrue(self.email.email_exists("non_existent_email"))
 
     @patch.object(Email, "fetch_data")
     def test_email_exists_false(self, mock_fetch_data):
+        # create a mock email instance of non_existent_email
         mock_fetch_data.return_value = []
-
+        # Assert false if email exists
         self.assertFalse(self.email.email_exists("example@example.com"))
 
     @patch.object(Email, "fetch_data")
@@ -288,23 +311,45 @@ class EmailTestCase(unittest.TestCase):
 
 class UserTestCase(unittest.TestCase):
     def setUp(self):
+        # Create a sample database connection of User instance
         self.user = User(
-            host='localhost', user='username', password='password', database='database'
+            host="localhost", user="username", password="password", database="database"
         )
 
-    @patch.object(User, 'execute_query')    
+    @patch.object(User, "execute_query")
     def test_insert_user_success(self, mock_execute_query):
-        mock_execute_query.return_value = (None)
-         
-        params = 'fullname','email', 'phone_no', 'city', 'state', 'hashed_password', 'salt', 'otp'
+        # Create a mock execute_query that is None
+        mock_execute_query.return_value = None
+        # Sample input parameters
+        params = (
+            "fullname",
+            "email",
+            "phone_no",
+            "city",
+            "state",
+            "hashed_password",
+            "salt",
+            "otp",
+        )
+        # Run insert_user method of User instance 
         self.user.insert_user(*params)
-
+        # Assert of mock execute_query is called
         mock_execute_query.assert_called_once()
-    
-    @patch.object(User, 'execute_query')
+
+    @patch.object(User, "execute_query")
     def test_insert_user_failure(self, mock_execute_query):
-        mock_execute_query.side_effect = Exception('database error')
-        params = 'fullname','email', 'phone_no', 'city', 'state', 'hashed_password', 'salt', 'otp'
+        # This test should fail if the user is already in the database
+        mock_execute_query.side_effect = Exception("database error")
+        params = (
+            "fullname",
+            "email",
+            "phone_no",
+            "city",
+            "state",
+            "hashed_password",
+            "salt",
+            "otp",
+        )
 
         # Call the create_customers_table method
         with self.assertLogs(level="ERROR") as log:
