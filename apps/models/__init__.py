576e24068864006b612dc603a51b171cdd881158
Amirhossein Dezhabdolahi <11095582-Amirhosseindzh@users.noreply.gitlab.com>
2024-06-04 16:41:26 +0330

    updated

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,3 +1,3 @@
-from apps.models.db_handler import DatabaseConnector, Email, Table, User
+from models.db_handler import DatabaseConnector, Email, Table, User, Flight
 
-__all__ = ['DatabaseConnector','Email', 'Table', 'User']
+__all__ = ['DatabaseConnector','Email', 'Table', 'User', 'Flight']
