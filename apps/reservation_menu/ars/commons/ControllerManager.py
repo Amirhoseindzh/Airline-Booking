128c53ca4dca01f469b79cf466ca9ba45ec6a989
Amirhossein Dezhabdolahi <11095582-Amirhosseindzh@users.noreply.gitlab.com>
2024-06-08 16:17:16 +0330

    sorted code

--- apps/reservation_menu/ars/commons/ControllerManager.py
+++ apps/reservation_menu/ars/commons/ControllerManager.py
@@ -1,16 +1,18 @@
 from reservation_menu.ars.commons.utils.ConsoleUtils import ConsoleUtils
-
 from reservation_menu.ars.commons.ControllerId import ControllerId
-from reservation_menu.ars.features.ListofPassengersController import ListofPassengersController
-from reservation_menu.ars.features.DisplayAirCraftController import DisplayAirCraftController
+from reservation_menu.ars.features.ListofPassengersController import (
+    ListofPassengersController,
+)
+from reservation_menu.ars.features.DisplayAirCraftController import (
+    DisplayAirCraftController,
+)
 from reservation_menu.ars.features.MenuController import MenuController
 from reservation_menu.ars.features.AlertController import AlertController
 from reservation_menu.ars.features.ReserveASeatController import ReserveASeatController
 
+
 class ControllerManager(object):
-
     def get_controller_by_id(self, id):
-
         if id == ControllerId.MENU:
             return MenuController()
         elif id == ControllerId.LIST_OF_PASSENGERS:
@@ -23,15 +25,12 @@ class ControllerManager(object):
             raise RuntimeError("Controller id not found")
 
     def run(self):
-
         current_id = ControllerId.MENU
 
         while True:
-
             ConsoleUtils.clear_screen()
 
             try:
-
                 current = self.get_controller_by_id(current_id)
 
                 current_id = current.display()
